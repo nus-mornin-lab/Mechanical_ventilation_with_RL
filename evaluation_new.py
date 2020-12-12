20322756d05ff84e661141ca88094df4e0f52911
liyanxuanandy@163.com <liyanxuanandy@163.com>
2020-12-12 17:52:00 +0800

    modify eval

--- evaluation_new.py
+++ evaluation_new.py
@@ -17,14 +17,14 @@ from datetime import datetime
 import setting
 
 
-def run_eval(data, loss=None):
-        
+def run_eval(data, loss, datatype, SEED = settings.SEED):
+
     def plot_loss(loss):
         if loss:
             plt.figure(figsize=(7,4))
             plt.plot(loss)
             plt.savefig(res_dir + 'loss.jpg',dpi = 100)
-    
+        
     def tag_conc_rate_and_diff_mean(dt):
         for v in action_types:
             dt[v + '_conc_rate'] = dt[v + '_conc'].mean()
@@ -119,21 +119,24 @@ def run_eval(data, loss=None):
         plt.tight_layout()
         plt.savefig(res_dir + 'stratified_action_distribution3.jpg',dpi = 200)        
     
-    def diff_vs_motality(data, outcome_col = 'hosp_mort'):
+    def diff_vs_outcome(data, outcome_col1 = 'hosp_mort',outcome_col2 = 'spo2_reach', bootstrap_round = 500):
     
     # vs_motality
         data = data.reset_index(drop = True).copy()
         # 4-hour level 
         x_s = {}
         res = {}
+        res_ = {}
         for v in action_types:
             x_s[v] = sorted(set(data[v + '_diff']))
             res[v] = {}
+            res_[v] = {}
             for k in x_s[v]:
                 res[v][k] = []
-                
-        used_data = data[[v + '_diff' for v in action_types] + [outcome_col]]
-        for i in range(2000):
+                res_[v][k] = []
+            
+        used_data = data[[v + '_diff' for v in action_types] + [outcome_col1, outcome_col2 ]]
+        for i in range(bootstrap_round):
             if i%10 == 0:
                 print ('bootstrap 4-hour level: ' + str(i) + '...')
 
@@ -141,10 +144,13 @@ def run_eval(data, loss=None):
             # visit level
             # diff vs motality
             for v in action_types:
+                # v = 'PEEP'
                 diff_col = v + '_diff'
-                aa = used_data.loc[df_index].groupby(diff_col).apply(lambda dt: dt[outcome_col].mean())
+                # aa = used_data.loc[df_index].groupby(diff_col).apply(lambda dt: dt[outcome_col1].mean())
+                aa1 = used_data.loc[df_index].groupby(diff_col).apply(lambda dt: dt[[outcome_col1,outcome_col2]].mean())
                 for k in x_s[v]:
-                    res[v][k].append(aa[k])
+                    res[v][k].append(aa1.loc[k,outcome_col1])
+                    res_[v][k].append(aa1.loc[k,outcome_col2])
             
         # patient level 
         x_s_a = {}
@@ -160,8 +166,9 @@ def run_eval(data, loss=None):
                 res_a[v][k] = []
             for k in x_s_b[v]:
                 res_b[v][k] = []
-        used_data = data[[v + '_diff_mean_level' for v in action_types] + [v + '_conc_rate_level' for v in action_types] + ['patientunitstayid',outcome_col]].drop_duplicates(subset = ['patientunitstayid'])
-        for i in range(2000):
+
+        used_data = data[[v + '_diff_mean_level' for v in action_types] + [v + '_conc_rate_level' for v in action_types] + ['patientunitstayid',outcome_col1 ]].drop_duplicates(subset = ['patientunitstayid'])
+        for i in range(bootstrap_round):
             if i%10 == 0:
                 print ('bootstrap: patient level: ' + str(i) + '...')
 
@@ -170,17 +177,18 @@ def run_eval(data, loss=None):
             for v in action_types:
                 diff_mean_col = v + '_diff_mean_level'
                 conc_rate_col = v + '_conc_rate_level'
-                aa = used_data.loc[df_index].groupby(diff_mean_col).apply(lambda dt: dt[outcome_col].mean())
-                bb = used_data.loc[df_index].groupby(conc_rate_col).apply(lambda dt: dt[outcome_col].mean())
+                aa = used_data.loc[df_index].groupby(diff_mean_col).apply(lambda dt: dt[outcome_col1].mean())
+                bb = used_data.loc[df_index].groupby(conc_rate_col).apply(lambda dt: dt[outcome_col1].mean())
                 for k in x_s_a[v]:
                     if k in aa.index:
-                        res_a[v][k].append(aa[k])        
+                        res_a[v][k].append(aa[k])  
                 for k in x_s_b[v]:
                     if k in bb.index:
-                        res_b[v][k].append(bb[k])          
+                        res_b[v][k].append(bb[k])  
+     
         
         # plot 
-        def plot_vs_motality(x_s, res, x_name, title_name,color_):
+        def plot_vs_outcome(x_s, res, x_name, y_name, title_name,color_):
             res_025 = {}
             res_500 = {}
             res_975 = {}
@@ -205,24 +213,29 @@ def run_eval(data, loss=None):
                 plt.grid()
                 plt.xlabel(x_name)
                 if i == 0:
-                    plt.ylabel('Motality')
+                    plt.ylabel(y_name)
                 plt.title(v)
             
             plt.savefig(res_dir + title_name +'.jpg',dpi = 200)
 
-        plot_vs_motality(x_s, res, 'Model action - Phys action', 'diff4h_vs_motality', 'red')
-        plot_vs_motality(x_s_a, res_a, 'Model action - Phys action', 'diffPatientlevel_vs_motality','brown')
-        plot_vs_motality(x_s_b, res_b, 'Concordant rate', 'conc_vs_motality','green')
+        plot_vs_outcome(x_s, res, 'Model action - Phys action', 'Motality', 'diff4h_vs_motality', 'red')
+        plot_vs_outcome(x_s_a, res_a, 'Model action - Phys action','Motality', 'diffPatientlevel_vs_motality','brown')
+        plot_vs_outcome(x_s_b, res_b, 'Concordant rate','Motality', 'conc_vs_motality','green')
+        
+        plot_vs_outcome(x_s, res_, 'Model action - Phys action','spo2_reach', 'diff4h_vs_spo2reach', 'pink')
 
-    def q_vs_motality(data, outcome_col = 'hosp_mort'):
+
+    def q_vs_outcome(data, outcome_col1 = 'hosp_mort', outcome_col2 = 'spo2_reach'):
         # Q_vs_motality    
         # data[phys_Q] = [random.uniform(-10,10) for i in range(len(data))]
         data = data.reset_index(drop = True).copy()    
         
         data[phys_Q + '_discre'] = data[phys_Q].apply(lambda x: round(x,2))
         Q_s = sorted(set(data[phys_Q + '_discre']))
-
-        bb = data.groupby(phys_Q + '_discre').apply(lambda dt: [dt[outcome_col].mean(),dt[outcome_col].sem()])
+        
+        # q vs motality #####################
+        bb = data.groupby(phys_Q + '_discre').apply(lambda dt: [dt[outcome_col1].mean(),dt[outcome_col1].sem()])
+    
         res_025_Q = []
         res_500_Q = []
         res_975_Q = []
@@ -244,9 +257,33 @@ def run_eval(data, loss=None):
         res_dt['bb'] = bb
         res_dt['dr'] = res_dt['bb'].apply(lambda x: str(round(x[0]*100,1)) + '+-' + str(round(x[1]*100,1)) + '%')
         
-        return res_dt
+        # q vs sop2_reach #####################
+        bb = data.groupby(phys_Q + '_discre').apply(lambda dt: [dt[outcome_col2].mean(),dt[outcome_col2].sem()])
+        
+        res_025_Q = []
+        res_500_Q = []
+        res_975_Q = []
+        for k in Q_s:
+            res_500_Q.append(bb[k][0])
+            res_025_Q.append(bb[k][0] - bb[k][1])
+            res_975_Q.append(bb[k][0] + bb[k][1])
+        plt.figure(figsize=(6,4))
+        line1, = plt.plot(Q_s,res_500_Q, color='lightblue', lw=1.5, ls='-', ms=4)
+        plt.fill_between(Q_s,res_025_Q, res_975_Q, color='lightblue', alpha=0.6)
+        # plt.xticks(Q_s)
+        # plt.grid()
+        plt.xlabel('Return of actions')
+        plt.ylabel('sop2 reach prob')
+        
+        plt.savefig(res_dir + 'q_vs_spo2.jpg',dpi = 200)
+        
+        res_dt_ = pd.DataFrame()
+        res_dt_['bb'] = bb
+        res_dt_['rr'] = res_dt_['bb'].apply(lambda x: str(round(x[0]*100,1)) + '+-' + str(round(x[1]*100,1)) + '%')
+        
+        return res_dt, res_dt_
     
-    def quantitive_eval(data,res_dt, outcome_col = 'hosp_mort'):
+    def quantitive_eval(data,res_dt,res_dt_, outcome_col = 'hosp_mort'):
         data['Random_action'] = [random.randrange(action_num) for i in range(len(data))]
         cc = data[phys_action].value_counts()
         data['One-size-fit-all_action'] = cc.index[np.argmax(cc)]
@@ -259,15 +296,15 @@ def run_eval(data, loss=None):
         for mod in ['ai','phys','Random','One-size-fit-all']:    
             q_dr_dt.loc[mod,'Q'] = data[mod + '_Q'].mean()
     
-        def find_nearest_Q(Q_mean):
+        def find_nearest_Q(Q_mean,res_dt):
             ind = np.argmin([abs(Q_mean - i) for i in res_dt.index])
             Q_res = res_dt.index[ind]
             return Q_res
         
         for mod in ['ai','phys','Random','One-size-fit-all']:
-            q_dr_dt.loc[mod,'dr'] = res_dt.loc[find_nearest_Q(q_dr_dt.loc[mod,'Q']),'dr']
-            
-        q_dr_dt.to_csv(res_dir + 'qmean_and_deathrate.csv', encoding = 'gb18030')
+            q_dr_dt.loc[mod,'deathrate'] = res_dt.loc[find_nearest_Q(q_dr_dt.loc[mod,'Q'] , res_dt),'dr']
+            q_dr_dt.loc[mod,'spo2reachrate'] = res_dt_.loc[find_nearest_Q(q_dr_dt.loc[mod,'Q'], res_dt_),'rr']
+        q_dr_dt.to_csv(res_dir + 'qmean_and_deathreachrate.csv', encoding = 'gb18030')
             
         return q_dr_dt
     
@@ -293,6 +330,7 @@ def run_eval(data, loss=None):
     phys_action = 'phys_action'
     ai_action = 'ai_action'
     
+    data['spo2_reach'] = data['next_ori_spo2'].apply(lambda x: (x >= 94 and x <= 98)+0 )
     if phys_action not in data.columns.tolist():
         data[phys_action] = data.apply(lambda x: int(x[action_types[0]+'_level']*9 + x[action_types[1]+'_level']*3 + x[action_types[2]+'_level']),axis = 1)
     data['ai_Q'] = np.max(data[Q_list],axis = 1)
@@ -319,13 +357,13 @@ def run_eval(data, loss=None):
     stratify_col = 'SOFA_level'
     data[stratify_col] = (data['ori_sofatotal'] <= 6)+0 
     
-    res_dir = 'result/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '_' + setting.REWARD_FUN + '/'
+    res_dir = 'result/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '_' + setting.REWARD_FUN + '_' + datatype +'_' + str(SEED) + '_' + setting.MODEL + '/'
     if os.path.isdir(res_dir) == False:
         os.makedirs(res_dir)            
     action_distribution3(data)
-    diff_vs_motality(data)
-    res_dt = q_vs_motality(data)
-    q_dr_dt = quantitive_eval(data, res_dt) 
+    diff_vs_outcome(data)
+    res_dt,res_dt_ = q_vs_outcome(data)
+    q_dr_dt = quantitive_eval(data, res_dt,res_dt_) 
     conc_dt = action_concordant_rate(data) 
     plot_loss(loss)
     print (q_dr_dt)
